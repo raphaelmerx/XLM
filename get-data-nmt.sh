a804bb8de78ab1b510a5c742c408ef122edc86f2
Raphael Merx <raphael.merx@gmail.com>
2019-09-20 15:11:40 +0900

    Only 50k parallel sentences, 10k BPE codes

--- get-data-nmt.sh
+++ get-data-nmt.sh
@@ -11,8 +11,8 @@ set -e
 #
 # Data preprocessing configuration
 #
-N_MONO=5000000  # number of monolingual sentences for each language
-CODES=60000     # number of BPE codes
+N_MONO=50000  # number of monolingual sentences for each language
+CODES=10000     # number of BPE codes
 N_THREADS=16    # number of threads in data preprocessing
 
 
